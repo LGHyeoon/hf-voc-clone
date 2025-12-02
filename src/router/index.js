5bbbaae9323ad3f329c7fffec9ccc3adeb7934dd
LGHyeoon <a01043355935@gmail.com>
2025-12-02 22:04:58 +0900

    라우팅 url 추가

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
+import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/components/Home.vue";
 import Register from "@/components/Register.vue";
-import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
-    { path: "/", component: Home },
-    { path: "/register", component: Register },
-]
+  { path: "/", name: "Home", component: Home },
+  { path: "/my-vocs", name: "myVoc", component: Register },
+  { path: "/login", name: "login", component: Register },
+  { path: "/register", name: "Register", component: Register },
+  { path: "/voc/register", name: "VocRegister", component: Register },
+  { path: "/voc/status", name: "VocStatus", component: Register },
+  { path: "/voc/faq", name: "VocFaq", component: Register },
+  { path: "/notice", name: "notice", component: Register },
+  { path: "/faq", name: "faq", component: Register },
+];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes
-})
+  history: createWebHistory(), // "/" 생략
+  routes,
+});
 
-export default router
+export default router;
