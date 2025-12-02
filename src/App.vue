23dfe97fc5d75d9ceb1f0d3c4812d9cd42865b06
LGHyeoon <a01043355935@gmail.com>
2025-12-02 22:04:27 +0900

    router-view 추가 및 불필요 코드 제거

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,14 @@
 <template>
   <div id="app">
-    <Header></Header>
-
+    <Header />
     <main>
+      <router-view />   <!-- 여기 확인 -->
     </main>
-
-    <Footer></Footer>
+    <Footer />
   </div>
 </template>
 
-<script>
-import Header from "./components/common/Header.vue";
-import Footer from "./components/common/Footer.vue";
-
-export default {
-  name: "App",
-  components: {
-    Header,
-    Footer,
-  }
-}
+<script setup>
+import Header from "@/components/common/Header.vue";
+import Footer from "@/components/common/Footer.vue";
 </script>
-
-<style>
-
-</style>
