eaecf18c9e4579a0e67852c8379946db7418495f
LGHyeoon <a01043355935@gmail.com>
2025-12-17 20:43:59 +0900

    현재 년도와 월로 자동 셋팅하도록 기능 추가

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -4,7 +4,7 @@
 			<!-- 날짜 부분 -->
 			<div class="flex items-center gap-4">
 				<button class="px-2 px-1 text-gray-500 hover:text-gray-800">◀</button>
-				<span class="text-lg font-semibold">2025.12</span>
+				<span class="text-lg font-semibold">{{ year }}.{{ month }}</span>
 				<button class="px-2 px-1 text-gray-500 hover:text-gray-800">▶</button>
 			</div>
 
@@ -13,3 +13,12 @@
 		</div>
 	</header>
 </template>
+
+<script setup>
+	import { ref } from 'vue'
+
+	const currentDate = ref(new Date());
+
+	const year = currentDate.value.getFullYear();
+	const month = currentDate.value.getMonth() + 1;
+</script>
