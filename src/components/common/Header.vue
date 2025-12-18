d747a6ad42c7a274ac0c5c4b1dd618b550d1826a
LGHyeoon <a01043355935@gmail.com>
2025-12-18 21:47:55 +0900

    로그인, 로그아웃 버튼 추가 및 pinia를 통한 상태 관리

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -8,17 +8,37 @@
 				<button class="px-2 px-1 text-gray-500 hover:text-gray-800">▶</button>
 			</div>
 
-			<!-- 지출 추가하는 버튼 -->
-			<button class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg text-sm font-medium">+ 지출 추가</button>
+			<div class="flex items-center gap-3">
+				<!-- 지출 추가하는 버튼 -->
+				<button class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg text-sm font-medium">+ 지출 추가</button>
+
+				<!-- 로그인, 로그아웃 버튼 -->
+				<button v-if="!auth.isLoggedIn" @click="doLogin" class="px-3 py-2 text-sm font-medium text-gray-600 hover:text-gray-900">로그인</button>
+				<button v-else @click="doLogout" class="px-3 py-2 text-sm font-medium text-red-600 hover:text-red-600">로그아웃</button>
+			</div>
 		</div>
 	</header>
 </template>
 
 <script setup>
-	import { ref } from 'vue'
+import { useUserStore } from '@/stores/auth';
+import { ref } from 'vue'
+import { useRouter } from 'vue-router';
 
-	const currentDate = ref(new Date());
+const currentDate = ref(new Date());
 
-	const year = currentDate.value.getFullYear();
-	const month = currentDate.value.getMonth() + 1;
+const year = currentDate.value.getFullYear();
+const month = currentDate.value.getMonth() + 1;
+
+const router = useRouter();
+const auth = useUserStore();
+
+const doLogin = () => {
+	router.push("/login");
+}
+
+const doLogout = () => {
+	auth.logout();
+	router.push("/");
+}
 </script>
